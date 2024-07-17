88ffec1840e00e708fc515717d9eca84701b7eb8
Rahul Raj Anand <rahulrajanand7@gmail.com>
2024-07-17 23:02:49 +0530

    updated App.jsx

---
+++ quiz-app/src/components/Quiz.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Quiz = () => {
+  return (
+    <div>
+        <p className='heading-txt'>QUIZ APP</p>
+        <div className='container'>
+          
+        </div>
+
+    </div>
+  )
+}
+
+export default Quiz
